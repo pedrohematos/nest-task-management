0a2eeebf3ec5d387e10a663b076224b44b300929
Pedro Matos <pedrohematos@outlook.com>
2022-01-17 12:49:14 -0300

    Implemented task ownership and restrictions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const port = 3000;
   await app.listen(port);
