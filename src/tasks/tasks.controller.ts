f173e55512efff07c331dba8e63361c0e4ef7960
Pedro Matos <pedrohematos@outlook.com>
2022-01-17 00:28:07 -0300

    corrected misspelling

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,7 +12,7 @@ import {
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { UpdateTaskStatusDto } from './dto/updtate-task-status.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 
